b2455339dc4cf4894010820587aa226bc1fa631c
nekosyakolya <greirex@mail.ru>
2017-03-14 19:52:48 +0300

    вынесение ширины и высоты

--- lw-3/js/CRectangle.js
+++ lw-3/js/CRectangle.js
@@ -5,6 +5,8 @@ function Rectangle()
     this.x2 = 80;
     this.y1 = 10;
     this.y2 = 40;
+    this.width = Math.abs(this.x1 - this.x2);
+    this.height = Math.abs(this.y1 - this.y2);
 }
 
 Rectangle.prototype = Object.create(Shape.prototype);
@@ -16,23 +18,25 @@ Rectangle.prototype.setCoordinates = function(x1, x2, y1, y2)
     this.x2 = x2;
     this.y1 = y1;
     this.y2 = y2;
+    this.width = Math.abs(this.x1 - this.x2);
+    this.height = Math.abs(this.y1 - this.y2);
 };
 
 Rectangle.prototype.draw = function(ctx)
 {
     ctx.fillStyle = this.getFillColor();
-    ctx.fillRect(this.x1, this.y1, Math.abs(this.x1 - this.x2), Math.abs(this.y1 - this.y2));
+    ctx.fillRect(this.x1, this.y1, this.width, this.height);
     ctx.lineWidth = 3;
     ctx.strokeStyle = this.getBorderColor();
-    ctx.strokeRect(this.x1, this.y1, Math.abs(this.x1 - this.x2), Math.abs(this.y1 - this.y2));
+    ctx.strokeRect(this.x1, this.y1, this.width, this.height);
 };
 
 Rectangle.prototype.calculateArea = function()
 {
-    return (Math.abs(this.x1 - this.x2) * Math.abs(this.y1 - this.y2)).toFixed(2);
+    return (this.width * this.height).toFixed(2);
 };
 
 Rectangle.prototype.calculatePerimeter = function()
 {
-    return ((Math.abs(this.x1 - this.x2) * 2) + (Math.abs(this.y1 - this.y2) * 2)).toFixed(2);
+    return ((this.width * 2) + this.height * 2)).toFixed(2);
 };
